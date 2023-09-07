c9dd9141d01d17510c419f1ac26351b3369aed7d
iwopant0825 <129718228+iwopant0825@users.noreply.github.com>
2023-09-07 18:37:23 +0900

    c1

---
+++ back.js
@@ -0,0 +1,38 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+// ctx.fillRect(25,25, 100, 100);
+// ctx.clearRect(45, 45, 60, 60);
+// ctx.strokeRect(50, 50, 50, 50);
+
+// ctx.beginPath();
+// ctx.moveTo(75,50);
+// ctx.lineTo(100,75);
+// ctx.lineTo(100,25);
+// ctx.fill();
+
+// ctx.fillStyle="red"
+// ctx.beginPath();
+// ctx.moveTo(0,0);
+// ctx.lineTo(75,0);
+// ctx.lineTo(0,75);
+// ctx.lineTo(0,0);
+// ctx.fill();
+//
+// ctx.strokeStyle="green"
+// ctx.beginPath();
+// ctx.moveTo(95,20);
+// ctx.lineTo(95,95);
+// ctx.lineTo(25,95);
+// ctx.lineTo(95,20);
+// ctx.stroke();
+
+// ctx.font="30px Arial";
+// var count = 0;
+// function run(){
+//     ctx.clearRect(0,0,canvas.width,canvas.height);
+//     count+=1;
+//     ctx.strokeText("안녕하십니끼",count,30,50);
+//     requestAnimationFrame(run)
+// }
+// requestAnimationFrame(run)
